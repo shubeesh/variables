c1891ba749928b83e91149704c2b52423072cdaf
shubeeth001 <shubeeth001@warren.k12.in.us>
2024-07-30 13:52:04 -0400

    var

---
+++ src/variables.java
@@ -0,0 +1,15 @@
+public class variables {
+
+    public static void main(String[] args) {
+        int FirstNumber = 5;
+        System.out.println(FirstNumber);
+        FirstNumber = 6;
+        System.out.println(FirstNumber);
+        FirstNumber = (10 + 5) + (2 * 10);
+        System.out.println(FirstNumber);
+        int firstnumber = 5;
+
+
+    }
+
+}
